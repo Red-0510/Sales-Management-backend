71a56b3f0cdcb2d7586bd4cb855fe1506363d0aa
Red-0510 <jaineshmachhi@gmail.com>
2023-04-27 00:22:06 +0530

    backend done

---
+++ routes/userRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {changePassword, forgotPassword, getUser, loginStatus, loginUser, logoutUser, registerUser, resetPassword, updateUser } from "../controllers/userController.js";
+import { userAuth } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/register",registerUser);
+router.post("/login",loginUser);
+router.get("/logout",logoutUser);
+router.get("/loggedin",loginStatus);
+router.get("/getuser",userAuth,getUser);
+
+router.patch("/updateuser",userAuth,updateUser)
+router.patch("/changepassword",userAuth,changePassword);
+router.post("/forgotpassword",forgotPassword);
+router.put("/resetpassword/:resetToken",resetPassword);
+
+export default router;

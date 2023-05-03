0773ec05cf46839f8cb9ecf86541f6910f71f6d1
Jainesh Machhi <69681031+Red-0510@users.noreply.github.com>
2023-05-04 00:00:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next){
   
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");

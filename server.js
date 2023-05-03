c89f3d28bbc0a898f768a291673a018f21b97402
Red-0510 <jaineshmachhi@gmail.com>
2023-05-03 22:40:19 +0530

    name changes

--- server.js
+++ server.js
@@ -1,69 +1,65 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import path from "path";
-
-// route imports
-import userRoute from "./routes/userRoute.js";
-import productRoute from "./routes/productRoute.js";
-import contactRoute from "./routes/contactRoute.js";
-
-
-// middleware functions
-import errorHandler from "./middlewares/error.js";
-
-dotenv.config();
-
-const __dirname = path.resolve();
+const express = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const { readdir } = require("fs");
+const morgan = require("morgan");
+const cors = require("cors");
+require("./models/User");
+// connecting to database and all databse connection related code is here
+const db = require("./db");
+let PORT = process.env.PORT || 8000;
 
 const app = express();
-
+app.use(morgan("dev"));
+// for recieving and sending json files
 app.use(express.json());
+// for cookie related usage
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+// for allowing cross origin requests
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 
-
-// setting cors and fixing the Access-Control-Allow-Origin error
-app.use(function (req, res, next){
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
+// app.use(login);
+app.use(function (req, res, next) {
+  const corsWhitelist = [
+    "https://y4sh-patel.github.io/tnp",
+    "https://tnp-svnit.herokuapp.com/#/",
+    "http://localhost:3000",
+  ];
+  if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, HEAD, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  //   res.header("Access-Control-Allow-Origin", "https://y4sh-patel.github.io/tnp");
+  //   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  //   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  //   res.header("Access-Control-Allow-Headers", "Content-Type");
+  //   res.header("Access-Control-Allow-Credentials", true);
+  // }
   next();
 });
 
-// serve the static files
-app.use("/uploads",express.static(path.join(__dirname,"uploads")));
+app.get("/", async (req, res) => {
+  res.json("Reached to the root");
+});
 
-//port
-const PORT = process.env.PORT || 5000;
+let pathname = __dirname + "/routes";
+//uses routes filename as the route name and the file as the route handler
+readdir(pathname, async (err, data) => {
+  if (err) console.log(err.message);
+  else
+    data.map((r) => {
+      return app.use(`/${r.split(".")[0]}`, require(`${pathname}/${r}`));
+    });
+});
 
-// mongoDB URL
-const databaseURL = process.env.DATABASE_URL_DEV;
-
-
-//adding routes
-app.use("/api/users",userRoute);
-app.use("/api/products",productRoute);
-app.use("/api/contactus",contactRoute);
-
-app.get("/",(req,res)=>res.send("Server working"));
-
-
-app.use(errorHandler);
-
-
-//connect to DB and start the server
-mongoose.connect(databaseURL)
-    .then(()=>{
-        app.listen(PORT,()=>{
-            console.log(`server is running on port: ${PORT}`);
-        });
-    })
-    .catch(err=>{
-        console.log(`Error connecting to Database: ${err.message}`);
-    })
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
